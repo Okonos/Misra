347277586c7bc4ecf456b0c4a5ece30d63b66d00
Okonos <tepyzboj@gmail.com>
2017-12-25 15:38:52 +0100

    Add command_center.py -- hotkeyed process signaling utility

---
+++ command_center.py
@@ -0,0 +1,36 @@
+import keyboard
+import subprocess
+import signal
+
+
+def sendSignal(nodeNum, signalNum):
+    signalToToken = {
+        '1': "PING",
+        '2': "PONG",
+    }
+    print("Commanding node{} to lose {} token".format(
+        nodeNum, signalToToken[signalNum]))
+
+    sshCmd = 'sudo -u okonos vagrant ssh node{} -c'.format(nodeNum)
+    cmd = 'ps aux | grep [p]ingpong | awk \'{print $2}\' | \
+        xargs kill -s SIGUSR' + signalNum
+    subprocess.call(sshCmd.split(' ') + [cmd])
+
+    print(40*"=")
+
+
+with open('./hosts', 'r') as f:
+    nodesNum = len(f.readlines())
+
+print("CTRL + node number - lose PING\n ALT + node number - lose PONG\n" +
+      "There are {} nodes".format(nodesNum))
+
+for i in range(1, nodesNum+1):
+    n = str(i)
+    keyboard.add_hotkey('ctrl+'+n, sendSignal, (n, '1'))
+    keyboard.add_hotkey('alt+'+n, sendSignal, (n, '2'))
+
+try:
+    signal.pause()
+except KeyboardInterrupt:
+    print("Quitting")
